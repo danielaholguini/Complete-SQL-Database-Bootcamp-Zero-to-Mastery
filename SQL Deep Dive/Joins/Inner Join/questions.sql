cad7550003591ae26332750d58755a1561358837
Daniela Holguin <106038804+danielaholguini@users.noreply.github.com>
2023-06-22 13:07:38 -0500

    Update questions.sql

--- SQL Deep Dive/Joins/Inner Join/questions.sql
+++ SQL Deep Dive/Joins/Inner Join/questions.sql
@@ -6,21 +6,30 @@
 * ordered by orderid
 */
 
-
+select * from orders o
+inner join customers c
+on o.customerid = c.customerid 
+where c.state  in ('OH', 'NY', 'OR')
+order by orderid
 
 /*
 * DB: Store
 * Table: products
 * Question: Show me the inventory for each product
 */
-
+select i.prod_id, i.quan_in_stock, p.title  from inventory i 
+join products p 
+on i.prod_id = p.prod_id 
 
 /*
 * DB: Employees
 * Table: employees
 * Question: Show me for each employee which department they work in
 */
-
+SELECT e.first_name, dp.dept_name
+FROM employees AS e
+INNER JOIN dept_emp AS de ON de.emp_no = e.emp_no
+INNER JOIN departments AS dp ON dp.dept_no = de.dept_no
 
 
 
